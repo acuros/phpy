bddea6f7b8d421dfde08b440385dabf29ec81da9
Joshua <seungyeon@joshua.kim>
2017-05-04 15:19:41 +0900

    Python3

--- phpy/php.py
+++ phpy/php.py
@@ -1,8 +1,27 @@
+from __future__ import print_function
 try:
     import simplejson as json
 except ImportError:
     import json
 from subprocess import Popen, PIPE
+
+from .exceptions import *
+
+
+try:
+   unicode = unicode
+except NameError:
+   # 'unicode' is undefined, must be Python 3
+   str = str
+   unicode = str
+   bytes = bytes
+   basestring = (str,bytes)
+else:
+   # 'unicode' exists, must be Python 2
+   str = str
+   unicode = unicode
+   bytes = str
+   basestring = basestring
 
 
 class PHP:
@@ -10,6 +29,8 @@ class PHP:
         self.file_path = file_path
 
     def __escape(self, string):
+        if isinstance(string, bytes):
+            string = string.decode('utf8')
         return string.replace('\\', '\\\\').replace("'", "\\'")
 
     def __quote(self, arguments):
@@ -33,12 +54,12 @@ class PHP:
         if (type(arguments)) != list and arguments:
             raise InvalidType
         p = Popen('php', stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        print >>p.stdin, '<?php '
-        print >>p.stdin, 'include \'%s\';' % self.__escape(self.file_path)
-        print >>p.stdin, '%s(%s);' % (function_name,
-                                    self.__join_arguments(arguments))
+        content = '<?php\ninclude \'{}\';\n{}({});?>'.format(
+            self.__escape(self.file_path), function_name, self.__join_arguments(arguments)
+        )
+        p.stdin.write(content.encode('utf8'))
         p.stdin.close()
-        result = p.stdout.read()
+        result = p.stdout.read().decode('cp949')
         if result.find('include(): Failed opening') > 0:
             raise PHPFileDoesNotExist(result)
         if result.find('Warning: ') > 0 and result.find('on line ') > 0:
@@ -50,4 +71,6 @@ class PHP:
         return self.__call_function(function_name, arguments)
 
     def get_dict(self, function_name, arguments=None):
-        return json.loads(self.__call_function(function_name, arguments))
+        result = self.__call_function(function_name, arguments)
+        print(result)
+        return json.loads(result)
